f8a5d476e6bea2d09eda5a18920490079b338c65
Ostromecki, Pawel (EXTERN: GOD) <pawel.ostromecki@god.dev>
2023-10-18 18:51:15 +0200

    feat: Add content to resume section

--- pages/Resume.vue
+++ pages/Resume.vue
@@ -7,7 +7,7 @@
       ></PageHeader>
     </section>
     <section class="flex flex-col sm:flex-row mb-16">
-      <div class="px-4 mb-16 sm:m-0">
+      <div class="px-4 mb-16 sm:m-0 sm:basis-1/2">
         <ResumeList :title="'Education'" :items="educationList">
           <template #default="{ item }">
             <h3>{{ item.title }}</h3>
@@ -16,8 +16,8 @@
           </template>
         </ResumeList>
       </div>
-      <div class="px-4">
-        <ResumeList :title="'Experience'" :items="educationList">
+      <div class="px-4 sm:basis-1/2">
+        <ResumeList :title="'Experience'" :items="experienceList">
           <template #default="{ item }">
             <h3>{{ item.title }}</h3>
             <span class="text-gray mb-2 block">{{ item.subtitle }}</span>
@@ -56,15 +56,62 @@
 const educationList = ref([
   {
     title: 'Computer Science',
-    subtitle: 'University of Wrocław',
+    subtitle: 'Wrocławska Wyższa Szkoła Informatyki Stosowanej 2017-2021',
+    content: 'BSE CS - Bachelor of Science in Engineering (Computer Science)'
+  }
+])
+
+const experienceList = ref([
+  {
+    title: 'Junior Software Developer',
+    subtitle: 'Exence S. A. (05.2018 - 12.2020)',
     content:
-      "I'm currently studying Computer Science at University of Wrocław. I'm in my 3rd year of studies."
+      'Software Development and maintenance (Angular 2+, Java Spring, SQL, Node, Nest.js, Electron, Vue) of enterprise apps dedicated for military, energy, manufacturing industries'
   },
   {
-    title: 'Computer Science',
-    subtitle: 'University of Wrocław',
-    content:
-      "I'm currently studying Computer Science at University of Wrocław. I'm in my 3rd year of studies."
+    title: 'Front-End Developer',
+    subtitle: 'Angry Nerds (01.2020 - 06.2020)',
+    content: `
+      Maintained app of Spanish airlines operator Volotea
+booking system, backoffice SPA, landing page
+ERP-class system development for commercial drinkg
+industry
+Stack: Angular 10+, Umbraco CMS
+
+      `
+  },
+  {
+    title: 'Full-Stack Developer',
+    subtitle: 'Sendit S. A. (07.2020 - 07.2021)',
+    content: `
+    Developed and maintained web app for leading polish
+delivery broker
+Developed backoffice SPA
+Developed map web widget using Vue.js and vanilla JS
+Stack: Angular 12, SS
+
+      `
+  },
+  {
+    title: 'Front-End Developer',
+    subtitle: 'GOD Nearshore (07.2021 - 10.2023)',
+    content: `
+    Developed and maintained enterprise ERP-class app for
+automotive industry
+Developed and maintained system consisted of 2 mobile
+apps created with Ionic. Old maintained in Ionic 3 +
+Cordova, new created from scratch with NX, Ionic 6,
+Capacitor 5`
+  },
+  {
+    title: 'Senior Front-End Developer (Freelancer)',
+    subtitle: 'IT Bees (06.2023 - 08.2023)',
+    content: `
+    Developed functionalities for brand new mobile app Park
+Control 24
+Integrated native functionalities on Android, iOS
+(camera, notifications, network etc.)
+Stack: Ionic 7, Capacitor 5, Angular 16`
   }
 ])
 </script>
